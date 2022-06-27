716942fd67942d5ed94c4f94dc68112246a57f92
ManuelTano <manueltano98@gmail.com>
2022-06-27 18:49:15 +0200

    fix Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 // Array di immagini
 
-const sources = ['./img/06.jpg','./img/07.jpg','./img/08.jpg','./img/09.jpg','./img/10.jpg'];
-
-const gallery = document.getElementById('gallery')
+const sources = ['img/06.jpg','img/07.jpg','img/08.jpg','img/09.jpg','img/10.jpg'];
 
 const maximage = document.getElementById('carosel');
+
 
 let imgContent = "";
 
 for (let i = 0; i < sources.length; i++) {
 
-    imgContent += `<div class="gallery"><img src"${sources[i]}" alt="0${i}"}/></div>`;
+    imgContent += `<img src="${sources[i]}" alt="0${i}"/>`;
 
     console.log(sources[i]);
 }
 
 
 maximage.innerHTML += imgContent;
+
+const imgElements = document.querySelectorAll('#carosel img');
+console.log(imgElements);
+
+let currentActiveIndex = 0;
+
+imgElements[currentActiveIndex].classList.add('active');
+
+
+
