c9ce20075767b0fab5d8b7f62e058e55b5e82e41
ManuelTano <manueltano98@gmail.com>
2022-06-27 17:51:09 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 const sources = ['./img/06.jpg','./img/07.jpg','./img/08.jpg','./img/09.jpg','./img/10.jpg'];
 
+const gallery = document.getElementById('gallery')
+
+const maximage = document.getElementById('carosel');
+
 let imgContent = "";
 
 for (let i = 0; i < sources.length; i++) {
@@ -12,5 +16,4 @@ for (let i = 0; i < sources.length; i++) {
 }
 
 
-const maximage = document.getElementById('carosel');
 maximage.innerHTML += imgContent;
